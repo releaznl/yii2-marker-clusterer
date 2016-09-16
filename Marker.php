caeaca98e66b96de49196a516fcd24b9c587a8f9
maxreleaz <max@releaz.nl>
2016-09-16 15:08:40 +0200

    model meegeven + globaal beschikbaar maken

--- Marker.php
+++ Marker.php
@@ -7,7 +7,7 @@ namespace edofre\markerclusterer;
  */
 class Marker extends \dosamigos\google\maps\overlays\Marker
 {
-
+	public $modelId;
 	/**
 	 * The constructor js code for the Marker object
 	 * @return string
@@ -15,10 +15,17 @@ class Marker extends \dosamigos\google\maps\overlays\Marker
 	public function getJs()
 	{
 		$js = $this->getInfoWindowJs();
-
+		
+		if(isset($this->modelId)){
+			$this->options['modelId'] = $this->modelId;
+		}
+		
 		$js[] = "var {$this->getName()} = new google.maps.Marker({$this->getEncodedOptions()});";
 		// add the marker to markers array
 		$js[] = "markers.push({$this->getName()});";
+		
+		// Make markers available globally
+		$js[] = "window.globalmarkers = markers;";
 
 		foreach ($this->events as $event) {
 			/** @var \dosamigos\google\maps\Event $event */
